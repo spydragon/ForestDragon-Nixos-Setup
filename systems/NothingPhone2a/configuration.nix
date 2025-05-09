9f37b9793cb9b2ea93a9708908b25d946edbced0
spydragon <52147734+spydragon@users.noreply.github.com>
2025-05-09 06:40:00 -0600

    added warning about system.stateVersion

--- systems/NothingPhone2a/configuration.nix
+++ systems/NothingPhone2a/configuration.nix
@@ -9,4 +9,16 @@
     ./Users/users.nix
     ../../modules/nixos/default-config.nix
   ];
+
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+
+  # PLEASE COPY FROM AUTO GENERATED configuration.nix FILE FROM INSTALLING NIXOS
+  #system.stateVersion = "24.11"; # Did you read the comment?
 }
